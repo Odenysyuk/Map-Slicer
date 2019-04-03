cae6d05c156ee478ab7f2c9d78eb3d464f471b6f
ovdenysiuk <olena.denysiuk@honeycombsoft.com>
2019-04-03 17:19:12 +0300

    Init project

---
+++ .tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var mapSlicerB1146AB518024EEF8B19C181A7ECC49E_DEBUG = {
+        name: 'mapSlicerB1146AB518024EEF8B19C181A7ECC49E_DEBUG',
+        displayName: 'MapSlicer',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '2.3.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.mapSlicerB1146AB518024EEF8B19C181A7ECC49E.Visual(options),
+        custom: true
+    };
+}
